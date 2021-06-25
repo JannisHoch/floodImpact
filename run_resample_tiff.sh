9dcc5d327848e0e679cef0bca5aa054dfd470c36
Jannis <j.m.hoch@uu.nl>
2021-06-25 16:54:05 +0200

    resampling as click function; added SSP2 analysis to nb

---
+++ run_resample_tiff.sh
@@ -0,0 +1,21 @@
+source_raster=./data/PCR_clipped_reprojected_boolean.tiff
+
+destination_raster=./data/original/population_SSP2_5min_bc_2018.tiff
+output_file=./data/population_SSP2_5min_bc_2018_resampled.tiff
+
+floodimpact --debug resample-tiff -out $output_file $destination_raster $source_raster
+
+destination_raster=./data/original/population_SSP2_5min_bc_2030.tiff
+output_file=./data/population_SSP2_5min_bc_2030_resampled.tiff
+
+floodimpact --debug resample-tiff -out $output_file $destination_raster $source_raster
+
+destination_raster=./data/original/population_SSP2_5min_bc_2040.tiff
+output_file=./data/population_SSP2_5min_bc_2040_resampled.tiff
+
+floodimpact --debug resample-tiff -out $output_file $destination_raster $source_raster
+
+destination_raster=./data/original/population_SSP2_5min_bc_2050.tiff
+output_file=./data/population_SSP2_5min_bc_2050_resampled.tiff
+
+floodimpact --debug resample-tiff -out $output_file $destination_raster $source_raster
